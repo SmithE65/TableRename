11cbc2cfb6aa095a5a17160057d5b0dac9c27f86
Eric Smith <ydrive65@yahoo.com>
2021-11-11 17:31:29 -0500

    Add project files.

---
+++ TableRename/ApplicationDbContext.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TableRename;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Entity1>? Entity1s { get; set; }
+    public DbSet<Entity2>? Entity2s { get; set; }
+    public DbSet<Entity3>? Entity3s { get; set; }
+}
+
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        var connectionString = @"server=(localdb)\MSSQLLocalDB;Database=TableRename;Integrated Security=true;Encrypt=False;";
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
+
+
+public class Entity1
+{
+    public int Id { get; set; }
+    public int Entity2Id { get; set; }
+    [ForeignKey(nameof(Entity2Id))]
+    public Entity2? Entity2Navigation { get; set; }
+    public int Entity3Id { get; set; }
+    [ForeignKey(nameof(Entity3Id))]
+    public Entity3? Entity3Navigation { get; set; }
+}
+
+public class Entity2
+{
+    public int Id { get; set; }
+    [InverseProperty(nameof(Entity1.Entity2Navigation))]
+    public ICollection<Entity1> Entity1s { get; set; } = new List<Entity1>();
+}
+
+public class Entity3
+{
+    public int Id { get; set; }
+    [InverseProperty(nameof(Entity1.Entity3Navigation))]
+    public ICollection<Entity1> Entity1s { get; set; } = new List<Entity1>();
+}
---
+++ TableRename/Migrations/20211111223100_Initial.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TableRename.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Entity2s",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entity2s", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Entity3s",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entity3s", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Entity1s",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Entity2Id = table.Column<int>(type: "int", nullable: false),
+                    Entity3Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entity1s", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Entity1s_Entity2s_Entity2Id",
+                        column: x => x.Entity2Id,
+                        principalTable: "Entity2s",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Entity1s_Entity3s_Entity3Id",
+                        column: x => x.Entity3Id,
+                        principalTable: "Entity3s",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Entity1s_Entity2Id",
+                table: "Entity1s",
+                column: "Entity2Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Entity1s_Entity3Id",
+                table: "Entity1s",
+                column: "Entity3Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Entity1s");
+
+            migrationBuilder.DropTable(
+                name: "Entity2s");
+
+            migrationBuilder.DropTable(
+                name: "Entity3s");
+        }
+    }
+}
